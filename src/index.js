e54cd0f9ed91a194419060358448036d7642aa63
vladimirm85 <vladimirm85@gmail.com>
2020-10-02 01:07:14 +0300

    feat: implement reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = (n) =>
+    n >= 0
+        ? Number(String(n).split("").reverse().join(""))
+        : Number(String(n).slice(1).split("").reverse().join(""));
